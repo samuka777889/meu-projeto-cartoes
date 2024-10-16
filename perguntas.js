26a24d9610ebf50f283467d5fd62b92371df6eb8
samuka777889 <samuelpedroso114@gmail.com>
2024-10-16 20:39:53 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Brasil',
+    'Em que ano o Brasil foi descoberto e por quem??',
+    'A chegada dos portugueses ao Brasil foi parte do processo das grandes navegações. Pedro Álvares Cabral foi o líder de uma expedição composta por 13 embarcações e cerca de 1200 homens. O primeiro ponto do Brasil avistado pelos portugueses foi a região do Monte Pascoal, na Bahia, no dia 22 de abril de 1500.'
+)
+
+criaCartao(
+    'Brasil',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasilia'
+)
+
+criaCartao(
+    'Brasil',
+    'Qual a capital do Paraná?',
+    'A capital do Paraná é Curitiba'
+)
+
+criaCartao(
+    'Brasil',
+    'Qual a capital do Para?',
+    'A capital da Para é Belem'
+)
+
+criaCartao(
+    'Brasil',
+    'Qual a capital de São Paulo?',
+    'A capital de São paulo é São Paulo'
+)
+
+criaCartao(
+    'Brasil',
+    'Qual a capital da Amazonas?',
+    'A capital da Amazonas é Manaus'
+)
